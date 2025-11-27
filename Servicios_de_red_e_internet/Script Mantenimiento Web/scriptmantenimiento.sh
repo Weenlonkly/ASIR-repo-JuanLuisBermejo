7258cc64b2b83a4af388f3d59c3c1fa8ff5c6706
Weenlonkly <133717274+Weenlonkly@users.noreply.github.com>
2025-11-27 21:04:43 +0100

    Update scriptmantenimiento.sh

--- Servicios_de_red_e_internet/Script Mantenimiento Web/scriptmantenimiento.sh
+++ Servicios_de_red_e_internet/Script Mantenimiento Web/scriptmantenimiento.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+carpeta_backups="/var/www/copias_de_seguridad"
+nombre_db="test"
+
+date=$(date +%Y%m%d_%H%M%S)
+
+copia_db="${nombre_db}_${date}.sql.gz"
+copia_html="web_html_${date}.tar.gz"
+
+# Crear carpeta si no existe
+mkdir -p "$carpeta_backups"
+
+# Backup web
+tar -czf "${carpeta_backups}/${copia_html}" /var/www/html
+
+# Backup base de datos
+mysqldump "$nombre_db" | gzip > "${carpeta_backups}/${copia_db}"
